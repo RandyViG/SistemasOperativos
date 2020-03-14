086000e15c558a8a9eceafa273169aaa06872679
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-03-14 10:57:02 -0600

    Agregando biblioteca para imprimir cadenas y numeros

--- Assembly/functions.asm
+++ Assembly/functions.asm
@@ -2,9 +2,11 @@
 ;	    Library for print strings and numbers
 ;**********************************************************
 
-;---------------------------------------------------------
+;----------------------------------------------------------
 ;	void	iprint( Integer number)
 ;	Integer	printing function (itoa)
+;----------------------------------------------------------
+
 iprint:
 	push	eax
 	push	ecx
@@ -38,6 +40,8 @@ printLoop:
 ;---------------------------------------------------------------
 ;	void iprintLF (Integer number)	
 ;	Integer printing function with linefeed ( itoi )
+;---------------------------------------------------------------
+
 iprintLF:
 	call	iprint
 	push	eax
@@ -52,9 +56,11 @@ iprintLF:
 ;---------------------------------------------------------------
 ;	int slen( String message )
 ;	String length calculation function
+;---------------------------------------------------------------
+
 slen:
 	push	ebx
-	mov	ebx,	eax
+	mov	ebx,		eax
 
 nextchar:
 	cmp	byte[eax],	0
@@ -63,13 +69,15 @@ nextchar:
 	jmp	nextchar
 
 finished:
-	sub	eax,	ebx
+	sub	eax,		ebx
 	pop	ebx
 	ret
 
 ;--------------------------------------------------------------
 ;	void sprint( String message )
 ;	String printing function
+;--------------------------------------------------------------
+
 sprint:
 	push	edx
 	push	ecx
@@ -77,12 +85,12 @@ sprint:
 	push	eax
 	call	slen
 
-	mov	edx,	eax
+	mov	edx,		eax
 	pop	eax
 	
-	mov	ecx,	eax
-	mov	ebx,	1
-	mov	eax,	4
+	mov	ecx,		eax
+	mov	ebx,		1
+	mov	eax,		4
 	int	0x80
 
 	pop	ebx
@@ -93,13 +101,15 @@ sprint:
 ;---------------------------------------------------------------
 ;	void sprintLF ( String message )
 ;	String printing with line feed dunctions
+;---------------------------------------------------------------
+
 sprintLF:
 	call	sprint
 
 	push	eax
-	mov	eax,	0x0A
+	mov	eax,		0x0A
 	push	eax
-	mov	eax,	esp
+	mov	eax,		esp
 	call	sprint
 	pop	eax
 	pop	eax
@@ -108,9 +118,48 @@ sprintLF:
 ;---------------------------------------------------------------
 ;	void exit()
 ;	Exit program and restore resource
+;---------------------------------------------------------------
 
 quit:
-	mov	ebx,	0
-	mov	eax,	1
+	mov	ebx,		0
+	mov	eax,		1
 	int	0x80
+	ret
+
+;---------------------------------------------------------------
+; 	int atoi (Integer number)
+; 	Ascii to integer function (atoi)
+;---------------------------------------------------------------
+
+atoi:
+	push	ebx
+	push	ecx
+	push	edx
+	push	esi
+	mov	esi,		eax
+	mov	eax,		0
+	mov	eax,		0
+
+.multiplyLoop:
+	xor	ebx,		ebx
+	mov	bl,		[esi+ecx]
+	cmp	bl,		48
+	jl	.finished
+	cmp	bl,		57
+	jg	.finished
+
+	sub	bl,		48
+	add	eax,		ebx
+	mov	ebx,		10
+	mul	ebx
+	inc	ecx
+	jmp	.multiplyLoop
+
+.finished:
+	mov	ebx,		10
+	div	ebx
+	pop	esi
+	pop	edx
+	pop	ecx
+	pop	ebx
 	ret
