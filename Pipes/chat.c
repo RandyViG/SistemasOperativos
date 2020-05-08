ed52e90cb30b1fa30a99d87c87ac3db9405b93f6
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-05-08 12:42:52 -0500

    Modificando indentación

--- Pipes/chat.c
+++ Pipes/chat.c
@@ -1,4 +1,4 @@
-// Mini chat entre proceso padre e hijo  utilizando PIPES
+/* Mini chat entre proceso padre e hijo  utilizando PIPES sin nombre */
 
 #include <stdio.h>
 #include <string.h>
@@ -21,8 +21,8 @@ int main ( void ){
 	if( pid == -1 )
 		printf("\tError al crear el proceso hijo... \n");
 	else if (pid == 0){
-    		close( dsDad[WRITE] );
-    		close( dsSon[READ] );
+    	close( dsDad[WRITE] ); /* Padre hacia el hjo */
+    	close( dsSon[READ] );  /* Hijo hacia el Padre */
 		while( 1 ){
 			memset( message , 0 , 100 );
 			memset( send , 0 , 100);
@@ -31,36 +31,36 @@ int main ( void ){
 				break;
  			printf("El hijo recibio: %s , %d bytes\n\n", message, bytes);
    
-    			printf("Mensaje hacia el padre: ");
-    			scanf(" %[^\n]s", send);
-    			write( dsSon[WRITE], send, strlen(send));
+    		printf("Mensaje hacia el padre: ");
+    		scanf(" %[^\n]s", send);
+    		write( dsSon[WRITE], send, strlen(send));
 			if( strncmp( end , send, 3 ) == 0 )
 				break;
 		}
-		printf("\tChat terminado (Proceso Hijo)... \n");
-    		close( dsSon[WRITE] );
+		printf("\tChat finalizado (Proceso Hijo)... \n");
+    	close( dsSon[WRITE] );
    		close( dsDad[READ] );
   	}
   	else{
-    		close( dsDad[READ] );
-    		close( dsSon[WRITE] );
+    	close( dsDad[READ] );  /* Padre hacia el hijo*/
+    	close( dsSon[WRITE] ); /* Hijo  hacia el padre*/
 		while( 1 ){
 			memset( message , 0 , 100 );
 			memset( send , 0 , 100);
-    			printf("Mensaje hacia el hijo: ");
-    			scanf(" %[^\n]s", send);
-			fflush(stdin);
-    			write( dsDad[WRITE],send , strlen(send));
+    		printf("Mensaje hacia el hijo: ");
+    		scanf(" %[^\n]s", send);
+    		write( dsDad[WRITE],send , strlen(send));
 			if( strncmp( end , send , 3 ) == 0 )
 				break;
-    			bytes = read(dsSon[READ], message, 100);
+
+    		bytes = read(dsSon[READ], message, 100);
 			if( strncmp( end , message , 3 ) == 0 )
 				break;
     			printf("El padre recibio: %s, %d bytes\n\n",message , bytes);
 		}
-		printf("\tChat terminado (Proceso padre)...\n");
-    		close( dsDad[WRITE] );
-    		close( dsSon[READ] );
+		printf("\tChat finalizado (Proceso padre)...\n");
+    	close( dsDad[WRITE] );
+    	close( dsSon[READ] );
   	}
 	return 0;
 }
