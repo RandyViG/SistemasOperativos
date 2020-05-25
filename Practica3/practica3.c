5774ebaf80e7c39d401605174c13981a337fdda4
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-05-25 18:19:46 -0500

    Quitando serialización al crear los procesos

--- Practica3/practica3.c
+++ Practica3/practica3.c
@@ -7,64 +7,59 @@
 void arbol( int rama, int nivel );
 
 int main( int argc, char *argv[] ){
-    int nivel,pid1,i;
+    int nivel,i;
+	pid_t pid1;
     if( argc < 2 ){
-		printf("Uso: %s niveles",argv[1]);
+		printf("Uso: %s niveles\n",argv[0]);
         exit(1);
     }
     nivel = atoi( argv[1] );
     for(i=0; i<2; i++) { 
         pid1 = fork();
         if( pid1 == -1)
-            perror("No se pudo crear el proceso...\n");
+            perror("Error!\n No se pudo crear el proceso: \n");
         else if( pid1 == 0 ){
             if( i == 0 ){
-				printf("RAMA IZQUIERDA\n");
-				printf("NIVEL %d \n",nivel);
-                printf("Hijo izquierdo mi id es %d y mi padres es %d\n", getpid(), getppid());
+                printf("Rama izquierda mi id es %d y mi padres es %d\n", getpid(), getppid());
                 arbol(1, --nivel);
             }   
             if( i == 1 ){
-				printf("\nRAMA DERECHA\n");
-				printf("NIVEL %d \n",nivel);
-                printf("Hijo derecho mi id es %d y mi padres es %d\n", getpid(), getppid());
+                printf("Rama derecha mi id es %d y mi padres es %d\n", getpid(), getppid());
                 arbol(2, --nivel);
             }
             else
                 exit(0);   
         }
-        else 
-            wait(0);
     }
+	wait(NULL);
     return 0;
 }
 
 
 void arbol( int rama, int nivel ){
-    int i,pid1;
+    int i;
+	pid_t pid1;
 	if(nivel <= 0)
 		return;
-	printf("NIVEL %d \n",nivel);
 	if( rama == 1){ //rama izquierda
-		for(i=0; i<2; i++) {	 
+		for(i=0; i<2; i++){	 
         	pid1 = fork();
         	if( pid1 == -1)
             	perror("No se pudo crear el proceso...\n");
         	else if( pid1 == 0 ){
             	if( i == 0 ){
-                	printf("Hijo izquierdo mi id es %d y mi padres es %d\n", getpid(), getppid());
+                	printf("Hijo izquierdo de rama izquierda mi id es %d y mi padres es %d\n", getpid(), getppid());
                 	arbol(1, --nivel);
             	}   
             	if( i == 1 ){
-                	printf("Hijo derecho mi id es %d y mi padres es %d\n", getpid(), getppid());
+                	printf("Hijo derecho de rama izquierda mi id es %d y mi padres es %d\n", getpid(), getppid());
                 	arbol(1, --nivel);
             	}
             	else
                 	exit(0);     
         	}
-            else 
-                wait(0);
     	}
+		wait(NULL);
 	}
 	if( rama == 2){ //rama derecha
 		for(i=0; i<3; i++){ 
@@ -73,22 +68,21 @@ void arbol( int rama, int nivel ){
     	        perror("No se pudo crear el proceso...\n");
     	    else if( pid1 == 0 ){
     	        if( i == 0 ){
-    	            printf("Hijo izquierdo mi id es %d y mi padres es %d\n", getpid(), getppid());
+    	            printf("Hijo izquierdo de rama derecha mi id es %d y mi padres es %d\n", getpid(), getppid());
     	            arbol(2, --nivel);
     	        }   
     	        if( i == 1 ){
-    	            printf("Hijo central mi id es %d y mi padres es %d\n", getpid(), getppid());
+    	            printf("Hijo central de rama derecha mi id es %d y mi padres es %d\n", getpid(), getppid());
     	            arbol(2, --nivel);
     	        }
     	        if( i == 2 ){
-    	            printf("Hijo derecho mi id es %d y mi padres es %d\n", getpid(), getppid());
+    	            printf("Hijo derecho de rama derecha mi id es %d y mi padres es %d\n", getpid(), getppid());
     	            arbol(2, --nivel);
     	        }
     	        else
     	            exit(0);
     	    }
-    	    else 
-    	        wait(0);
     	}
+		wait(NULL);
 	}
 }
